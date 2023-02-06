7e452026604c3243c2fa83ca47e79b7cee85787b
JS <49812749+JS00001@users.noreply.github.com>
2023-02-06 12:04:22 -0500

    Add images, fix menu component, custom colors

--- src/pages/dashboard/generate-labels.tsx
+++ src/pages/dashboard/generate-labels.tsx
@@ -9,16 +9,29 @@ import QRCode from "react-qr-code";
 import classNames from "classnames";
 import html2canvas from "html2canvas";
 import { useState, useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 import api from "@/api";
 import withAuth from "@/hoc/withAuth";
 import Layout from "@/components/Layout";
 import Button from "@/components/Button";
-import { useMutation } from "@tanstack/react-query";
+
+import LabelsBlue from "@/assets/LabelsBlue";
+import LabelsPink from "@/assets/LabelsPink";
+import LabelsYellow from "@/assets/LabelsYellow";
+import LabelsOrange from "@/assets/LabelsOrange";
+import Menu from "@/components/Menu";
 
 const DashboardGenerateLabelsPage: React.FC = () => {
   const divRef = useRef<HTMLDivElement>(null);
+
+  const [menuOpen, setMenuOpen] = useState(false);
   const [labels, setLabels] = useState<string[]>([]);
+  const [selectedColor, setSelectedColor] = useState<"blue" | "pink" | "yellow" | "orange">("blue");
+
+  const handleMenuToggle = () => {
+    setMenuOpen((prev) => !prev);
+  };
 
   const createLabelSheetMutation = useMutation({
     mutationFn: async () => {
@@ -32,7 +45,6 @@ const DashboardGenerateLabelsPage: React.FC = () => {
         const labelData = data.labels.map((label) => `trw://${label.uniqueID}`);
         setLabels(labelData);
       },
-      onError: (error) => {},
     });
   };
 
@@ -65,15 +77,56 @@ const DashboardGenerateLabelsPage: React.FC = () => {
       <Layout.Header>Generate Labels</Layout.Header>
       <Layout.Content className="mt-10">
         <div className="flex items-center gap-x-5">
+          <Menu open={menuOpen} position="bottom">
+            <Menu.Item
+              title="Blue"
+              onClick={() => {
+                setSelectedColor("blue");
+                handleMenuToggle();
+              }}
+            />
+            <Menu.Item
+              title="Pink"
+              onClick={() => {
+                setSelectedColor("pink");
+                handleMenuToggle();
+              }}
+            />
+            <Menu.Item
+              title="Yellow"
+              onClick={() => {
+                setSelectedColor("yellow");
+                handleMenuToggle();
+              }}
+            />
+            <Menu.Item
+              title="Orange"
+              onClick={() => {
+                setSelectedColor("orange");
+                handleMenuToggle();
+              }}
+            />
+            <Menu.Button>
+              <Button onClick={handleMenuToggle} iconRight="IoChevronDown">
+                {selectedColor === "blue"
+                  ? "Color - Blue"
+                  : selectedColor === "pink"
+                  ? "Color - Pink"
+                  : selectedColor === "yellow"
+                  ? "Color - Yellow"
+                  : "Color - Orange"}
+              </Button>
+            </Menu.Button>
+          </Menu>
           <Button
-            iconLeft="IoColorWand"
+            iconRight="IoColorWand"
             onClick={handleGenerateLabelSheet}
             loading={createLabelSheetMutation.isLoading}
           >
             Generate Label Sheet
           </Button>
           <Button
-            iconLeft="IoCodeDownloadOutline"
+            iconRight="IoCodeDownloadOutline"
             onClick={onDownloadLabelSheet}
             disabled={lodash.isEmpty(labels) || createLabelSheetMutation.isLoading}
           >
@@ -83,7 +136,7 @@ const DashboardGenerateLabelsPage: React.FC = () => {
 
         {labels.length > 0 && (
           <div className="mt-4 w-fit rounded-md border border-gray-200 p-10 ">
-            <div className="grid h-full grid-cols-3 grid-rows-2 gap-4 ">
+            <div className="grid h-full grid-cols-3 grid-rows-4 gap-4 ">
               {labels.map((label, index) => (
                 <div
                   className={classNames(
@@ -104,24 +157,33 @@ const DashboardGenerateLabelsPage: React.FC = () => {
         )}
 
         {labels.length > 0 && (
-          <div id="offscreenLabels" ref={divRef} className="hidden h-[1008px] w-[1772px]">
-            <div className="grid h-full grid-cols-3 grid-rows-2 ">
-              {labels.map((label, index) => (
-                <div
-                  className={classNames(
-                    "flex",
-                    index % 3 === 0
-                      ? "justify-start"
-                      : index % 3 === 1
-                      ? "justify-center"
-                      : "justify-end",
-                    index < 3 ? "items-start" : "items-end"
-                  )}
-                >
-                  <QRCode size={256} value={label.toString()} />
+          <div id="offscreenLabels" ref={divRef} className="relative hidden">
+            {selectedColor === "blue" && <LabelsBlue />}
+            {selectedColor === "pink" && <LabelsPink />}
+            {selectedColor === "yellow" && <LabelsYellow />}
+            {selectedColor === "orange" && <LabelsOrange />}
+            {labels.length > 0 && (
+              <div className="absolute top-[169px] left-[169px] h-[1008px] w-[886px]">
+                <div className="grid h-full grid-cols-3 grid-rows-4 gap-y-[372px] ">
+                  {labels.map((label, index) => {
+                    return (
+                      <div
+                        className={classNames(
+                          "flex",
+                          index % 3 === 0
+                            ? "justify-start"
+                            : index % 3 === 1
+                            ? "justify-center"
+                            : "justify-end"
+                        )}
+                      >
+                        <QRCode size={128} value={label.toString()} />
+                      </div>
+                    );
+                  })}
                 </div>
-              ))}
-            </div>
+              </div>
+            )}
           </div>
         )}
       </Layout.Content>
@@ -129,4 +191,4 @@ const DashboardGenerateLabelsPage: React.FC = () => {
   );
 };
 
-export default withAuth(DashboardGenerateLabelsPage);
+export default DashboardGenerateLabelsPage;
